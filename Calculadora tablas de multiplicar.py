a96e457821f7f6ebec9e7e3b17cbc8a20d859b82
LeticiaOlivera <lx02683@innovaccion.mx>
2023-09-24 20:34:17 -0600

    Agregue primer programa en la carpeta

---
+++ Calculadora tablas de multiplicar.py
@@ -0,0 +1,4 @@
+tablas = lambda x : [x * 1, x * 2, x * 3, x * 4, x * 5, x * 6, x * 7, x * 8, x * 9, x * 10]
+
+numero = int(input('Ingresa el número del que deseas la tabla: '))
+print('La tabla de multiplicar es: ', tablas(numero))
